d0e269affe777933e894f5ce4b316cc6c9948ce3
Luke Hagar <lukeslakemail@gmail.com>
2024-02-28 09:44:13 -0500

    un-ignore build

---
+++ build/Types.js
@@ -0,0 +1,11 @@
+export const NOT_LANGUAGES = [
+    'html',
+    'markdown',
+    'dockerfile',
+    'roff',
+    'rich text format',
+    'powershell',
+    'css',
+    'php',
+];
+//# sourceMappingURL=Types.js.map
