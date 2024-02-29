80c0a2fffbc23ac6a958b7ad9d613c7f1c6b6016
luke-hagar-sp <98849695+luke-hagar-sp@users.noreply.github.com>
2024-02-29 18:55:03 -0500

    Added handling for errored repo queries

--- src/index.ts
+++ src/index.ts
@@ -364,10 +364,15 @@ try {
     }
   }
 
-  const contributorStats = (await Promise.all(contributorStatsPromises))
+  const contributorStats = (await Promise.allSettled(contributorStatsPromises))
     .filter((entry) => entry !== null || entry !== undefined)
     .map((entry) => {
-      return (Array.isArray(entry.data) ? entry.data : [entry.data])
+      if (entry.status === "rejected") {
+        return [];
+      }
+      return (
+        Array.isArray(entry.value.data) ? entry.value.data : [entry.value.data]
+      )
         .filter(
           (contributor) => contributor.author?.login === userDetails.data.login
         )
